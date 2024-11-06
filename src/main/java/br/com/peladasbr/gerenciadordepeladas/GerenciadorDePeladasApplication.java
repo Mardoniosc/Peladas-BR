3b795ff6f0c8662fb736775670e9f97a77f8f51e
Mardonio S Costa <35425708+Mardoniosc@users.noreply.github.com>
2024-11-06 12:34:26 -0300

    PROJETO CRIADO

---
+++ src/main/java/br/com/peladasbr/gerenciadordepeladas/GerenciadorDePeladasApplication.java
@@ -0,0 +1,13 @@
+package br.com.peladasbr.gerenciadordepeladas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDePeladasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDePeladasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/peladasbr/gerenciadordepeladas/GerenciadorDePeladasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.peladasbr.gerenciadordepeladas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePeladasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
